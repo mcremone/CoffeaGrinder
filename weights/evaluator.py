b82cf51456ff2d0ad1a51aab99f4c6a7f33108e7
Lindsey Gray <Lindsey.Gray@cern.ch>
2018-11-21 19:59:11 -0800

    can now also accept tuples of same-sized jagged arrays

--- weights/evaluator.py
+++ weights/evaluator.py
@@ -1,4 +1,6 @@
 import numpy as np
+from awkward.array.jagged import JaggedArray
+import awkward as ak
 from copy import deepcopy
 
 class denselookup(object):
@@ -14,9 +16,39 @@ class denselookup(object):
         self.__axes = deepcopy(dims)
         self.__values = deepcopy(values)
         self.__type = type(self.__values)
-        
+    
     def __call__(self,*args):        
-        indices = []
+        inputs = list(args)
+        print inputs
+        counts = None
+        print
+        for i in xrange(len(inputs)):
+            print i,type(inputs[i])
+            if isinstance(inputs[i], JaggedArray):
+                if counts is not None and set(counts) != set(inputs[i].counts) and len(counts) == len(inputs[i].counts):
+                    if counts == -1:
+                        raise Exception('do not mix JaggedArrays and numpy arrays when calling denselookup')
+                    else:
+                        raise Exception('counts not uniform between all input jagged arrays!')
+                counts = inputs[i].counts
+                inputs[i] = inputs[i].flatten()
+                print type(inputs[i])
+            elif isinstance(inputs[i], np.ndarray):
+                counts = -1
+        retval = self.__evaluate(*tuple(inputs))
+        for arg in args:
+            if isinstance(arg, JaggedArray):
+                retval = JaggedArray.fromcounts(arg.counts,retval)
+                break
+        print retval
+        print
+        return retval
+                                               
+    
+    def __evaluate(self,*args):        
+        indices = [] 
+        for arg in args: 
+            if type(arg) == JaggedArray: raise Exception('JaggedArray in inputs')
         if self.__dimension == 1:
             indices.append(np.maximum(np.minimum(np.searchsorted(self.__axes, args[0], side='right')-1,self.__values.shape[0]-1),0))
         else:
@@ -24,7 +56,6 @@ class denselookup(object):
                 print self.__axes[dim], self.__values.shape
                 indices.append(np.maximum(np.minimum(np.searchsorted(self.__axes[dim], args[dim], side='right')-1,self.__values.shape[len(self.__axes)-dim-1]-1),0))
         indices.reverse()
-        print tuple(indices)
         return self.__values[tuple(indices)]
 
 class evaluator(object):
