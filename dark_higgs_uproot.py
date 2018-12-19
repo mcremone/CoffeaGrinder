3cb9a660c4c9dd2d30bb761741f0ea83c0886a3e
Matteo Cremonesi <matteoc@mac-132345.guest-network.net>
2018-12-19 17:45:07 -0600

    keeping improving

--- dark_higgs_uproot.py
+++ dark_higgs_uproot.py
@@ -5,6 +5,46 @@ from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
 
 f = "nano_5.root"
 
+met_trigger_paths = ["HLT_PFMET170_NoiseCleaned",
+            "HLT_PFMET170_HBHECleaned",
+            "HLT_PFMET170_JetIdCleaned",
+            "HLT_PFMET170_NotCleaned",
+            "HLT_PFMET170_HBHE_BeamHaloCleaned",
+            "HLT_PFMETNoMu120_NoiseCleaned_PFMHTNoMu120_IDTight",
+            "HLT_PFMETNoMu110_NoiseCleaned_PFMHTNoMu110_IDTight",
+            "HLT_PFMETNoMu90_NoiseCleaned_PFMHTNoMu90_IDTight",
+            "HLT_PFMETNoMu90_PFMHTNoMu90_IDTight",
+            "HLT_PFMETNoMu100_PFMHTNoMu100_IDTight",
+            "HLT_PFMETNoMu110_PFMHTNoMu110_IDTight",
+            "HLT_PFMETNoMu120_PFMHTNoMu120_IDTight"]
+met_trigger_columns = Table({path:path for path in met_trigger_paths.items()})
+
+singleele_trigger_paths = ["HLT_Ele27_WP85_Gsf",
+          "HLT_Ele27_WPLoose_Gsf",
+          "HLT_Ele105_CaloIdVT_GsfTrkIdT",
+          "HLT_Ele27_WPTight_Gsf",
+          "HLT_Ele30_WPTight_Gsf",
+          "HLT_Ele27_eta2p1_WPTight_Gsf",
+          "HLT_Ele32_eta2p1_WPTight_Gsf",
+          "HLT_Ele35_WPLoose_Gsf",
+          "HLT_ECALHT800"]
+singleele_trigger_columns = Table({path:path for path in singleele_trigger_paths.items()})
+
+singlephoton_triggerpaths = ["HLT_Photon175",
+          "HLT_Photon200",
+          "HLT_Photon165_HE10",
+          "HLT_Photon36_R9Id90_HE10_IsoM",
+          "HLT_Photon50_R9Id90_HE10_IsoM",
+          "HLT_Photon75_R9Id90_HE10_IsoM",
+          "HLT_Photon90_R9Id90_HE10_IsoM",
+          "HLT_Photon120_R9Id90_HE10_IsoM",
+          "HLT_Photon165_R9Id90_HE10_IsoM",
+          "HLT_Photon300_NoHE",
+          "HLT_ECALHT800",
+          "HLT_CaloJet500_NoJetID"]
+singlephoton_trigger_columns = Table({path:path for path in singlephoton_trigger_paths.items()})
+
+
 electron_columns = {'pt':'Electron_pt','eta':'Electron_eta','phi':'Electron_phi','mass':'Electron_mass','iso':'Electron_pfRelIso03_all','dxy':'Electron_dxy','dz':'Electron_dz','id':'Electron_mvaSpring16GP_WP90'}
 
 muon_columns = {'pt':'Muon_pt','eta':'Muon_eta','phi':'Muon_phi','mass':'Muon_mass','iso':'Muon_pfRelIso04_all','dxy':'Muon_dxy','dz':'Muon_dz'}
@@ -20,10 +60,18 @@ columns = []
 for cols in all_columns: columns.extend(list(cols.values()))
 
 for arrays in uproot.iterate(f,'Events',columns,entrysteps=50000):
-        electrons = JaggedCandidateArray.candidatesfromcounts(arrays[electron_columns['pt']].counts, **{key:arrays[val].content for key,val in electron_columns.items()})
-        muons = JaggedCandidateArray.candidatesfromcounts(arrays[muon_columns['pt']].counts, **{key:arrays[val].content for key,val in muon_columns.items()})
-        taus = JaggedCandidateArray.candidatesfromcounts(arrays[tau_columns['pt']].counts, **{key:arrays[val].content for key,val in tau_columns.items()})
+
+        triggers = JaggedCandidateArray.candidatesfromcounts(arrays[met_trigger_columns['HLT_PFMET170_NoiseCleaned']].counts, **{MET:np.prod([arrays[val].content for val in met_trigger_columns]),SingleEle:np.prod([arrays[val].content for val in singleelectron_trigger_columns]),SinglePhoton:np.prod([arrays[val].content for val in singlephoton_trigger_columns])}
         
+        electrons = JaggedCandidateArray.candidatesfromcounts(arrays[electron_columns['pt']].counts, **{key:arrays[val].content for key,val in electron_columns.items()})
+                                                             
+        muons = JaggedCandidateArray.candidatesfromcounts(arrays[muon_columns['pt']].counts, **{key:arrays[val].content for key,val in muon_columns.items()})
+                                                             
+        taus = JaggedCandidateArray.candidatesfromcounts(arrays[tau_columns['pt']].counts, **{key:arrays[val].content for key,val in tau_columns.items()})
+                                                             
+        photons = JaggedCandidateArray.candidatesfromcounts(arrays[tau_columns['pt']].counts, **{key:arrays[val].content for key,val in photon_columns.items()})
+                                                             
+        jets = JaggedCandidateArray.candidatesfromcounts(arrays[tau_columns['pt']].counts, **{key:arrays[val].content for key,val in jet_columns.items()})
 
 loose_electron_selection = (electrons.pt>7)*(abs(electrons.eta)<2.4)*(abs(electrons.dxy)<0.05)*(abs(electrons.dz)<0.2)*(electrons.iso<0.4)*(electrons.id)
 loose_muon_selection =  (muons.pt>5)*(abs(muons.eta)<2.4)*(abs(muons.dxy)<0.5)*(abs(muons.dz)<1.0)*(muons.iso<0.4)
