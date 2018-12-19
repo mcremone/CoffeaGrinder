c11a506275e723bd5a4e29662c2c18cea5d84ee1
fnavarro94 <felinavarro94@gmail.com>
2018-12-19 16:26:33 -0600

    switched dummy for real sf evaluator

--- dark_higgs_uproot.py
+++ dark_higgs_uproot.py
@@ -3,13 +3,22 @@ import numpy as np
 import fnal_column_analysis_tools
 from fnal_column_analysis_tools.analysis_objects import JaggedCandidateArray
 
+
+extractor = fnal_column_analysis_tools.lookup_tools.extractor()
+extractor.add_weight_sets(['* * eleTrig.root'])
+extractor.finalize()
+
+evaluator = extractor.make_evaluator()
+
+
+
 f = "nano_5.root"
 
 electron_columns = {'pt':'Electron_pt','eta':'Electron_eta','phi':'Electron_phi','mass':'Electron_mass','iso':'Electron_pfRelIso03_all','dxy':'Electron_dxy','dz':'Electron_dz','id':'Electron_mvaSpring16GP_WP90'}
 
 muon_columns = {'pt':'Muon_pt','eta':'Muon_eta','phi':'Muon_phi','mass':'Muon_mass','iso':'Muon_pfRelIso04_all','dxy':'Muon_dxy','dz':'Muon_dz'}
 
-all_columns [electron_columns,muon_columns]
+all_columns = [electron_columns,muon_columns]
 columns = []
 for cols in all_columns: columns.extend(list(cols.values()))
 
@@ -22,10 +31,10 @@ loose_e_selection = (electrons.pt>7)*(abs(electrons.eta)<2.4)*(abs(electrons.dxy
 loose_electrons = electrons[loose_e_selection]
 e_counts = loose_electrons.counts
 e_sfTrigg = np.ones(loose_electrons.size)
-e_sfTrigg[e_counts>0] = 1 - dummySF(loose_electrons.eta[e_counts>0,0], loose_electrons.pt[e_counts > 0,0])
-e_sfTrigg[e_counts > 1] =  1- (1- dummySF(loose_electrons.eta[e_counts>1,0], loose_electrons.pt[e_counts > 1,0]))*(1- dummySF(loose_electrons.eta[e_counts>1,1], loose_electrons.pt[e_counts > 1,1]))
+e_sfTrigg[e_counts>0] = 1 - evaluator["hEffEtaPt"](loose_electrons.eta[e_counts>0,0], loose_electrons.pt[e_counts > 0,0])
+e_sfTrigg[e_counts > 1] =  1- (1- evaluator["hEffEtaPt"](loose_electrons.eta[e_counts>1,0], loose_electrons.pt[e_counts > 1,0]))*(1- evaluator["hEffEtaPt"](loose_electrons.eta[e_counts>1,1], loose_electrons.pt[e_counts > 1,1]))
 
-
+#print evaluator["hEffEtaPt"](loose_electrons.eta[e_counts>0,0], loose_electrons.pt[e_counts > 0,0])
 
 
 
